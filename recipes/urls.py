b687d2b34aa29ce54c626faa25f251a7be4141d8
Thiago Fernandes <lalaaks22@gmail.com>
2025-05-28 20:32:37 -0300

    criando modelos

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipes)
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipes, name="recipe"),
    
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -6,6 +6,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html',context={'recipes': [make_recipe()for _ in range(10)]})
 
 def recipes(request, id):
-    return render(request, 'recipes/pages/recipe-views.html',context={'recipe': [make_recipe()]})
+    return render(request, 'recipes/pages/recipe-views.html',context={'recipe': make_recipe(),
+    'is_detail_page': True,
+})
+        
 
 
